1caede8c141f3e82df49bfac431e61632089349b
Nikita <69157423+nktshklv-dev@users.noreply.github.com>
2022-06-17 12:48:28 +0300

    Initial Commit

---
+++ Swifty-Words-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty-Words-Practice
+//
+//  Created by Nikita  on 6/17/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
